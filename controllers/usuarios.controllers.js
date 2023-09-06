97bb4bac2717b9a526bf833ae708f22abca0226e
Andreea <andelenasamur@gmail.com>
2023-09-06 10:01:24 +0200

    validate that the email is unique

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,27 +1,50 @@
+const { response } = require('express');
+
 const Usuario = require('../models/usuario');
 
 
-const getUsuarios =  (req, res) => {
+const getUsuarios = async (req, res) => {
+
+    const usuarios = await Usuario.find({}, 'nombre email role google');
+
 
     res.json({
         ok: true,
-        msg: 'get Usuarios'
+        usuarios
     });
 
 }
 
-const crearUsuario = async (req, res) => {
+const crearUsuario = async (req, res = response) => {
 
     const { email, password, nombre } = req.body;
 
-    const usuario = new Usuario(req.body);
+    try {
 
-    await usuario.save();
+        const existeEmail = await Usuario.findOne({email});
 
-    res.json({
-        ok: true,
-        usuario
-    });
+        if( existeEmail ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya esta registrado'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado... revisar logs'
+        });
+    }
 
 }
 
