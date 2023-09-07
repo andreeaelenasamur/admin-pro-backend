0b16e23c9e48c72229b0651f689f66e661d4f61c
Andreea <andelenasamur@gmail.com>
2023-09-07 14:02:41 +0200

    CRUD - from doctors

--- models/medico.js
+++ models/medico.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const HospitalSchema = Schema({
+const MedicoSchema = Schema({
 
     nombre: {
         type: String,
@@ -13,11 +13,15 @@ const HospitalSchema = Schema({
         type: Schema.Types.ObjectId,
         ref: 'Usuario'
     },
-}, { collection: 'hospitales' });
+    hospital: {
+        type: Schema.Types.ObjectId,
+        ref: 'Hospital'
+    }
+});
 
-HospitalSchema.method('toJSON', function() {
+MedicoSchema.method('toJSON', function() {
     const { __v, ...object} = this.toObject();
     return object;
 })
 
-module.exports = model('Hospital', HospitalSchema);
+module.exports = model('Medico', MedicoSchema);
---
+++ routes/medicos.routes.js
@@ -0,0 +1,38 @@
+/* 
+    Medicos
+    ruta: '/api/medico'
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos.controller');
+
+const router = Router();
+
+router.get( '/', getMedicos );
+
+router.post( 
+    '/', 
+    [
+
+    ],
+    crearMedico
+);
+
+router.put(
+    '/:id',
+    [
+
+    ],
+    actualizarMedico
+);
+
+router.delete(
+    '/:id',
+    borrarMedico
+)
+
+module.exports = router;
