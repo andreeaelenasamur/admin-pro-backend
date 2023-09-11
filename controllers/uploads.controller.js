509edc0d7b680f978d03c7b7a60744205f617e47
Andreea <andelenasamur@gmail.com>
2023-09-11 09:52:28 +0200

    assign the image to a doctor

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,4 +1,6 @@
 const { response } = require("express");
+const { v4: uuidv4 } = require('uuid');
+const { actualizarImagen } = require("../helpers/actualizar-imagen");
 
 const fileUpload = ( req, res= response ) => {
 
@@ -23,14 +25,47 @@ const fileUpload = ( req, res= response ) => {
     }
 
     // Procesar la imagen...
+    const file = req.files.imagen;
+
+    const nombreCortado = file.name.split('.');
+    const extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+    // Validar extensión
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    if( !extensionesValidas.includes(extensionArchivo) ) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'No es una extensión permitida'
+        });
+    }
+
+    // Generar el nombre del archivo
+    const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
+
+    // Path para guardar la imagen
+    const path = `./uploads/${tipo}/${nombreArchivo}`;
 
 
+    // Mover la imagen
+    file.mv(path, (err) => {
+        if (err){
+            console.log(err)
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error al mover la imagen'
+            });
+        }
 
+        // Actualizar base de datos
+        actualizarImagen(tipo, id, nombreArchivo);
 
-    res.json({
-        ok:true,
-        msg: 'fileUploaded'
-    })
+        res.json({
+            ok:true,
+            msg: 'Archivo subido',
+            nombreArchivo
+        });
+    });
+
 }
 
 module.exports = {
---
+++ helpers/actualizar-imagen.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const Usuario = require('../models/usuario');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
+const actualizarImagen = async(tipo, id, nombreArchivo) => {
+
+    switch( tipo ) {
+        case 'medicos':
+            const medico = await Medico.findById(id);
+            if ( !medico ) {
+                console.log('No es un médico por id')
+                return false;
+            }
+
+            const pathViejo = `./uploads/medicos/${medico.img}`;
+            console.log(fs.existsSync(pathViejo))
+            console.log(pathViejo);
+            if ( fs.existsSync(pathViejo) ) {
+                // borrar la imagen anterior
+                fs.unlinkSync(pathViejo);
+            }
+
+            medico.img = nombreArchivo;
+            await medico.save();
+            return true;
+
+            break;
+        case 'hospitales':
+
+            break;
+        case 'usuarios':
+
+            break;
+        
+    }
+
+
+
+}
+
+module.exports = {
+    actualizarImagen
+}
