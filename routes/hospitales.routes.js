d2b9efd9069a2ead5ea50202ef69d4fd20a797ad
Andreea <andelenasamur@gmail.com>
2023-09-11 17:01:17 +0200

    update hospital

--- routes/hospitales.routes.js
+++ routes/hospitales.routes.js
@@ -27,7 +27,9 @@ router.post(
 router.put(
     '/:id',
     [
-
+        validarJWT,
+        check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+        validarCampos
     ],
     actualizarHospital
 );
